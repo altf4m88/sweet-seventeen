03ecc743f50eda8f288ea255bc55e252a218a06e
altf4m88 <fadhilmauladhani@gmail.com>
2022-01-05 16:36:14 +0700

    add present svg

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const scene = new THREE.Scene();
 
+const TL = new TimelineMax();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+    canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMapping = THREE.ReinhardToneMapping;
-renderer.toneMappingExposure = 2.3;
+renderer.toneMappingExposure = 2;
 renderer.shadowMap.enabled = true;
 
-camera.position.setZ(5);
-camera.position.setY(0);
+camera.position.setZ(15);
+camera.position.setY(10);
 camera.position.setX(0);
 
 renderer.render(scene, camera);
@@ -60,8 +62,8 @@ let loader = new THREE.GLTFLoader();
 loader.crossOrigin = true;
 loader.load( '../models/cake_3d/scene.gltf', function ( data ) {
     var object = data.scene;
-    object.position.set(0, -4, -10);
-    object.rotation.set(Math.PI / 15, 11, 0);
+    object.position.set(0, 5, 0);
+    object.rotation.set(Math.PI / 15, 11, -0.4);
 
     object.traverse(n => { if ( n.isMesh ) {
         n.castShadow = true; 
@@ -75,7 +77,7 @@ loader.load( '../models/cake_3d/scene.gltf', function ( data ) {
 
 const loadHeart = () => {
     loader.load( '../models/low_poly_spinning_heart/scene.gltf', function ( data ) {
-        var object = data.scene;
+        let object = data.scene;
     
         object.traverse(n => { if ( n.isMesh ) {
             n.castShadow = true; 
@@ -86,13 +88,30 @@ const loadHeart = () => {
         const [x, y] = Array(3)
         .fill()
         .map(() => THREE.MathUtils.randFloatSpread(40));
+
+        let randomNum = Math.floor((Math.random() * 10) + 3);
+        let randomZ = Math.floor((Math.random() * -10) - 18);
     
-        object.position.set(x, y, -15);
+        object.position.set(x, y, randomZ);
+        TweenMax.fromTo(object.position, randomNum,
+            {
+                y: y - 30,
+                yoyo: true,
+                repeat: -1,
+                ease: 'Power2.easeInOut'
+            },
+            {
+                y: Math.floor((Math.random() * 20)),
+                yoyo: true,
+                repeat: -1,
+                ease: 'Power2.easeInOut'
+            }
+        );
         scene.add( object );
     });
 }
 
-Array(10).fill().forEach(loadHeart);
+Array(20).fill().forEach(loadHeart);
 
 let composer = new POSTPROCESSING.EffectComposer(renderer);
 composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
@@ -110,18 +129,34 @@ const effectPass = new POSTPROCESSING.EffectPass(
 effectPass.renderToScreen = true;
 composer.addPass(effectPass);
 
-// const controls = new THREE.OrbitControls(camera, renderer.domElement);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+controls.rotateSpeed = 0.3;
+controls.zoomSpeed = 0.9;
+
+controls.minDistance = 10;
+controls.maxDistance = 20;
+
+controls.minPolarAngle = 0; // radians
+controls.maxPolarAngle = Math.PI /2; // radians
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
 
 function animate() {
     composer.render();
     requestAnimationFrame(animate);
+
+    // hearts.forEach((object) => {
+    //     object.rotation.y += 0.025;
+    // });
 
     spotLight.position.set(
         camera.position.x + 10,
         camera.position.y + 10,
         camera.position.z + 10,
     )
-    // controls.update()
+    controls.update()
     // renderer.render(scene, camera);
 }
 
